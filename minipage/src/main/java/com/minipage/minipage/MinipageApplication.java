392f91ef84ad1361ed40f6f0a290cba69e2c045d
Micoko <112613372+Micoko@users.noreply.github.com>
2023-10-13 14:18:43 +0900

    init

---
+++ minipage/src/main/java/com/minipage/minipage/MinipageApplication.java
@@ -0,0 +1,13 @@
+package com.minipage.minipage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinipageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinipageApplication.class, args);
+	}
+
+}
---
+++ minipage/src/test/java/com/minipage/minipage/MinipageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minipage.minipage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinipageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
